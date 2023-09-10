d75dfc0f9f9b731970cc06a44beccd03ee7ad8c6
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-09-10 17:19:46 -0300

    1

---
+++ Palavras-Reservadas/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+
+    //Palavras reservadas
+    /*
+     * Controle de pacotes "import, package"
+     * 
+     * Modificadores de acesso "public, private, protected"
+     * primitivos "boolean, byte, char,-
+     * -double, float, int, long, short, void"
+     * 
+     * Modificadores de classes "abstract, class, extends, final, implements,
+     * interface, native, new, static, strictfn, synchronized,
+     * transient, volatile, "
+     *
+     * controle de fluxo dentro de um bloco de código "break, case, continue"
+     *
+     *  
+     */
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!"); 
+    }
+}
